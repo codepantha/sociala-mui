167d6c72b9914f3ace9a9f46af4c1531ee9a19a0
codepanther <pezechigozirim@gmail.com>
2022-05-11 01:26:53 +0100

    complete navigation

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,22 +1,107 @@
-import React from 'react'
-import { AppBar, styled, Toolbar, Typography } from '@mui/material'
-import { ConnectWithoutContact } from '@mui/icons-material';
-
+import React, { useState } from 'react';
+import {
+  AppBar,
+  Avatar,
+  Badge,
+  Box,
+  InputBase,
+  Menu,
+  MenuItem,
+  styled,
+  Toolbar,
+  Typography
+} from '@mui/material';
+import {
+  ConnectWithoutContact,
+  Mail,
+  Notifications
+} from '@mui/icons-material';
+import { borderRadius } from '@mui/system';
 
 const StyledToolbar = styled(Toolbar)({
   display: 'flex',
   justifyContent: 'space-between'
 });
 
+const Search = styled('div')(({ theme }) => ({
+  backgroundColor: 'white',
+  padding: '0 10px',
+  width: '40%',
+  borderRadius: theme.shape.borderRadius
+}));
+
+const Icons = styled(Box)(({ theme }) => ({
+  display: 'none',
+  alignItems: 'center',
+  gap: '20px',
+  [theme.breakpoints.up('sm')]: {
+    display: 'flex'
+  }
+}));
+
+const UserBox = styled(Box)(({ theme }) => ({
+  display: 'flex',
+  alignItems: 'center',
+  gap: '10px',
+  [theme.breakpoints.up('sm')]: {
+    display: 'none'
+  }
+}));
+
 const Navbar = () => {
+  const [open, setOpen] = useState(false);
+
   return (
     <AppBar position="sticky">
       <StyledToolbar>
-        <Typography variant="h6" sx={{ display: {xs: 'none', sm: 'block' }}}>Sociala</Typography>
-        <ConnectWithoutContact sx={{ display: { xs: 'block', sm: 'none' }}} />
+        <Typography variant="h6" sx={{ display: { xs: 'none', sm: 'block' } }}>
+          Sociala
+        </Typography>
+        <ConnectWithoutContact sx={{ display: { xs: 'block', sm: 'none' } }} />
+        <Search>
+          <InputBase placeholder="search..." />
+        </Search>
+        <Icons>
+          <Badge badgeContent={4} color="error">
+            <Mail />
+          </Badge>
+          <Badge badgeContent={2} color="error">
+            <Notifications />
+          </Badge>
+          <Avatar
+            sx={{ width: '30px', height: '30px' }}
+            src="https://images.pexels.com/photos/2379004/pexels-photo-2379004.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            onClick={() => setOpen(true)}
+          />
+        </Icons>
+        <UserBox onClick={() => setOpen(true)}>
+          <Avatar
+            sx={{ width: '30px', height: '30px' }}
+            src="https://images.pexels.com/photos/2379004/pexels-photo-2379004.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          />
+          <Typography variant="span">Ocean</Typography>
+        </UserBox>
       </StyledToolbar>
+      <Menu
+        id="demo-positioned-menu"
+        aria-labelledby="demo-positioned-button"
+        open={open}
+        onClose={() => setOpen(false)}
+        anchorOrigin={{
+          vertical: 'top',
+          horizontal: 'right'
+        }}
+        transformOrigin={{
+          vertical: 'top',
+          horizontal: 'left'
+        }}
+      >
+        <MenuItem>Profile</MenuItem>
+        <MenuItem>My account</MenuItem>
+        <MenuItem>Logout</MenuItem>
+      </Menu>
     </AppBar>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
